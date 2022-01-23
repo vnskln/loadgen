0a13eabf0d9c4e560cc5927a6525579a9b9d2c02
vnskln <vnskln.dev@gmail.com>
2022-01-24 00:08:08 +0100

    memory load test

---
+++ src/main/java/com/pb/loadgen/loadcontrollers/MemLoadController.java
@@ -0,0 +1,30 @@
+package com.pb.loadgen.loadcontrollers;
+
+import com.pb.loadgen.domains.LoadInput;
+import com.pb.loadgen.loadgenerators.Salesman;
+import com.pb.loadgen.loadgenerators.StubbornSalesman;
+import com.pb.loadgen.loadgenerators.IndecisiveSalesman;
+
+public class MemLoadController implements LoadController {
+
+    LoadInput loadInput;
+    byte[] loadArray;
+
+    public MemLoadController(LoadInput loadInput) {
+        this.loadInput = loadInput;
+    }
+
+    @Override
+    public void generate() {
+        System.out.println("Starting load generator");
+        loadArray = new byte [1000*1000*100];
+        System.out.println("Load generator started");
+    }
+
+    @Override
+    public void stopGenerating() {
+        loadArray = null;
+        System.gc();
+        System.out.println("Load generator stopped");
+    }
+}
