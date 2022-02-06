679627c1593bd101e74c8caab2616ab8f8185db3
vnskln <vnskln.dev@gmail.com>
2022-02-06 15:22:47 +0100

    chore: lombok, logging fix: indecisive salesman has low and high treshold fix: indecisive salesman load change cycle time is properly counted

--- src/main/java/com/pb/loadgen/controllers/HomeController.java
+++ src/main/java/com/pb/loadgen/controllers/HomeController.java
@@ -5,6 +5,7 @@ import com.pb.loadgen.domains.LoadType;
 import com.pb.loadgen.loadcontrollers.CpuLoadController;
 import com.pb.loadgen.loadcontrollers.LoadController;
 import com.pb.loadgen.loadcontrollers.MemLoadController;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+@Slf4j
 @Controller
 @SessionAttributes("loadInput")
 public class HomeController {
@@ -28,19 +30,23 @@ public class HomeController {
 
     @GetMapping("/")
     public String home (@ModelAttribute LoadInput loadInput) {
+        log.info("Front - load picker");
         return "home";
     }
     
     @PostMapping("/loadDetails")
     public String loadDetails (@ModelAttribute LoadInput loadInput) {
+        log.info("Front - load details");
         return "load_details";
     }
 
     @PostMapping("/start")
     public String start (@ModelAttribute LoadInput loadInput) throws InterruptedException {
         if (loadInput.getLoadType() == LoadType.MEM_COLLECTOR) {
+            log.info("Front - preparing memory load");
             loadController = new MemLoadController(loadInput);
         } else {
+            log.info("Front - preparing CPU load");
             loadController = new CpuLoadController(loadInput);
         }
         loadController.generate();
@@ -49,6 +55,7 @@ public class HomeController {
 
     @GetMapping("/stop")
     public String stop () {
+        log.info("Front - load generator stop");
         loadController.stopGenerating();
         return "stop";
     }
